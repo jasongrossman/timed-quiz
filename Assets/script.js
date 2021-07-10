41029691b26c6fd7207eaaddf3b823da4e69777b
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-07-10 13:51:51 -0400

    added pseudocode to structure game logic

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,41 @@
+//global variable declarations
 var launcherEl = document.querySelector("#launcher");
+var highScoresList = document.querySelector("#high-scores");
+var QuestionContent = [];
 
+//Quiz function
 var quizHandler = function() {
+    
+    //Display Instructions
     window.alert("Hello world");
+
+    //loop to iterate through each question in the questionContentArray
+
+    //create Container to hold elements 
+
+
+    //create Question element
+
+
+    //Create Answer Buttons
+
+    
+    //Begin Countdown Timer
+
+    
+    //Pull question #1 from QuestionContent Array, and display all values
+
+    //if function to validate correct answer
+
+
 }
 
+//High Scores List
+var displayHighScores = function() {
+    window.alert("You are good at this game!");
+}
+
+//event listeners
 launcherEl.addEventListener("click", quizHandler);
+highScoresList.addEventListener("click", displayHighScores);
 
