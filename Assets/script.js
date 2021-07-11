1e081e3068825b3990b13157c08b0ddacc6fa03b
jasongrossman <85508030+jasongrossman@users.noreply.github.com>
2021-07-11 13:55:38 -0400

    created div element for quiz container and appended to HTML

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,34 @@
 //global variable declarations
 var launcherEl = document.querySelector("#launcher");
 var highScoresList = document.querySelector("#high-scores");
-var QuestionContent = [];
+var questionCounter = 0;
+var questionContent = [ 
+    {
+        question: "Does this work?",
+        answerA: "Doubt it",
+        answerB: "Yes!",
+        answerC: "lmao no",
+        answerD: "r u srs?",
+    },
+    {
+        question: "Who are you?",
+        answerA: "Jason",
+        answerB: "Bjorn",
+        answerC: "Mr. Frog",
+        answerD: "Dr. Pepper",
+    }
+];
+
+var solutions = [
+    {
+       question: 1,
+       solution: "Yes!"
+    },
+    {
+        question: 2,
+        solution: "Jason"
+    }
+];
 
 //Quiz function
 var quizHandler = function() {
@@ -12,13 +39,37 @@ var quizHandler = function() {
     //loop to iterate through each question in the questionContentArray
 
     //create Container to hold elements 
+    var questionBox = document.createElement("div");
+    questionBox.className = "question-box";
+    questionBox.id = "question-box";
+    document.getElementById("quiz").appendChild(questionBox);
 
+    var questionDisplay = document.createElement("h2");
+    var questionDisplayText = document.createTextNode(questionContent[0].question);
+    questionDisplay.appendChild(questionDisplayText);
+    
+    document.getElementById("question-box").appendChild(questionDisplay);
+
+
+    //remove start quiz button
+    launcher.remove();
+
+
+    // var questionBoxEl = document.createElement("div");
+    // questionBoxEl.classname = "question-box";
+    // questionBoxEl.innerHTML = "<h3> Are you there? </h3>";
+    // questionBoxEl.appendChild("quiz");
+
+    // var questionDisplay = document.createElement("h2");
+    // questionDisplay.className = "question-display"; 
+    // questionBoxEl.appendChild(questionDisplay);
+    // questionDisplay.setAttribute("question-id", questionCounter)
 
     //create Question element
-
+    console.log(questionContent[0].question);
 
     //Create Answer Buttons
-
+    console.log(solutions[0].solution);
     
     //Begin Countdown Timer
 
